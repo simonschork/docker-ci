c733d8ffdff0221e22d454a14db7cefe71b54a69
Simon Schork <simon.schork@gmail.com>
2020-05-24 13:43:30 +0200

    tried creating website for api

---
+++ website/index.php
@@ -0,0 +1,24 @@
+
+<html>
+    <head>
+        <title>Weine</title>
+    </head>
+
+    <body>
+        <h1>Weinauswahl:</h1>
+        <ul>
+            <?php
+
+            $json = file_get_contents('http://weine-service/');
+            $obj = json_decode($json);
+
+            $weine = $obj->products;
+
+            foreach ($weine as $wein) {
+                echo "<li>$wein</li>";
+            }
+
+            ?>
+        </ul>
+    </body>
+</html>
